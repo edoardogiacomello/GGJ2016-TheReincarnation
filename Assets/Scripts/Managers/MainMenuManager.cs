18df0fbb71ef846b0ca94a543b5cd942ab6b93d2
FlexoChiurlo <stefano.e.fiorentini@gmail.com>
2016-01-31 10:46:54 +0100

    Main Menu Finished
    Added all the buttons in the main menu: it should be ok now

--- Assets/Scripts/Managers/MainMenuManager.cs
+++ Assets/Scripts/Managers/MainMenuManager.cs
@@ -1,9 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour {
     public static MainMenuManager instance;
+    public Button[] startButton;
+    public Button[] gameButton;
   
     void Awake()
     {
@@ -15,7 +18,30 @@ public class MainMenuManager : MonoBehaviour {
     void Start () {
 	
 	}
-	
+    public void seeGameButton() {
+        foreach ( Button b in startButton)
+        {
+            b.gameObject.SetActive(false);
+        }
+        foreach (Button bb in gameButton)
+        {
+            bb.gameObject.SetActive(true);
+        }
+    }
+
+    public void seeStartButton()
+    {
+        foreach (Button b in gameButton)
+        {
+            b.gameObject.SetActive(false);
+        }
+        foreach (Button bb in startButton)
+        {
+            bb.gameObject.SetActive(true);
+        }
+    }
+
+
     public void GoToLevel(string levelName)
     {
         if (levelName == "exitgame")
